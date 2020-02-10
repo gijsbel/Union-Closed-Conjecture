5461d2f398c6b8820d684490bed974caba0bcc2b
Gijs Bellaard <gijsleb@hotmail.com>
2020-02-10 16:02:44 +0100

    second

---
+++ core.py
@@ -0,0 +1,106 @@
+from itertools import chain, combinations
+from random import sample
+import math
+
+def familyFromList(L):
+    return set(frozenset(i) for i in L)
+
+# pretty prints a family
+def printFamily(family):
+    print([list(m) for m in family])
+
+# checks if a family is union closed
+def isUnionClosed(family):
+    for m in family:
+        for n in family:
+            if m.union(n) not in family:
+                #print("The union of "+str(list(m))+" and "+str(list(n))+" is not in the family")
+                return False
+    return True
+
+# make a family union-closed
+def makeUnionClosed(family):
+    c = family.copy()
+    for m in family:
+        for n in family:
+            c.add(m.union(n))
+    return c
+
+# finds how many members of the family contain a certain element
+def numberOfAppearances(family, element):
+    n = 0
+    for m in family:
+        if element in m:
+            n += 1
+    return n
+
+# returns the set that contains all relevant elements of the family
+def getGroundPlane(family):
+    a = set()
+    for m in family:
+        a = a.union(m)
+    return a
+
+# finds the most common elements in the family
+def mostCommon(family):
+    a = getGroundPlane(family);
+    m = set()
+    n = 0
+    for e in a:
+        k = numberOfAppearances(family,e)
+        #print(str(e)+" appears "+str(k)+" times")
+        if k==n:
+            m.add(e)
+        if k>n:
+            n = k
+            m = set()
+            m.add(e)
+    return m
+
+# checks if the most apparant element is in at least
+# half of the members of the members of the family
+def hasCommon(family):
+    e = mostCommon(family)
+    if(len(e)==0):
+        return False
+    return numberOfAppearances(family,e.pop())>=len(family)/2
+
+
+# https://stackoverflow.com/questions/1482308/how-to-get-all-subsets-of-a-set-powerset
+def powerset(aset):
+    s = list(aset)
+    p = set()
+    for r in range(len(s)+1):
+        p = p.union(set(frozenset(i) for i in combinations(s, r)))
+    return p
+
+# finds the minimal members
+# that is, the members that do contain properly contain another member
+# except for the empty set
+def minimalMembers(family):
+    minimalset = set()
+    for m in family:
+        minimal = True
+        for n in family:
+            if len(n)>0 and (not n is m):
+                if n.issubset(m):
+                    minimal = False
+                    break
+        if minimal:
+            minimalset.add(m)
+    return minimalset
+
+# union of the minimal members
+def minimalElements(family):
+    minElements = set()
+    minMembers = minimalMembers(family)
+    for m in minMembers:
+        for e in m:
+            minElements.add(e)
+    return minElements
+
+#chooses k random subsets from the set {1, ..., n}
+#every subset has the same probability of being chosen
+def randomFamily(n,k):
+    P = powerset(range(1,n+1))
+    return set(sample(P,k))
--- unionclosed.py
+++ unionclosed.py
@@ -1,122 +1,23 @@
-from itertools import chain, combinations
-from random import sample
-import math
-
-def familyFromList(L):
-    return set(frozenset(i) for i in L)
-
-# pretty prints a family
-def printFamily(family):
-    print([list(m) for m in family])
-
-# checks if a family is union closed
-def isUnionClosed(family):
-    for m in family:
-        for n in family:
-            if m.union(n) not in family:
-                #print("The union of "+str(list(m))+" and "+str(list(n))+" is not in the family")
-                return False
-    return True
-
-# make a family union-closed
-def makeUnionClosed(family):
-    c = family.copy()
-    for m in family:
-        for n in family:
-            c.add(m.union(n))
-    return c
-
-# finds how many members of the family contain a certain element
-def numberOfAppearances(family, element):
-    n = 0
-    for m in family:
-        if element in m:
-            n += 1
-    return n
-
-# returns the set that contains all relevant elements of the family
-def getGroundPlane(family):
-    a = set()
-    for m in family:
-        a = a.union(m)
-    return a
-
-# finds the most common elements in the family
-def mostCommon(family):
-    a = getGroundPlane(family);
-    m = set()
-    n = 0
-    for e in a:
-        k = numberOfAppearances(family,e)
-        #print(str(e)+" appears "+str(k)+" times")
-        if k==n:
-            m.add(e)
-        if k>n:
-            n = k
-            m = set()
-            m.add(e)
-    return m
-
-# checks if the most apparant element is in at least
-# half of the members of the members of the family
-def hasCommon(family):
-    e = mostCommon(family)
-    if(len(e)==0):
-        return False
-    return numberOfAppearances(family,e.pop())>=len(family)/2
+from core import *
 
 
-# https://stackoverflow.com/questions/1482308/how-to-get-all-subsets-of-a-set-powerset
-def powerset(aset):
-    s = list(aset)
-    p = set()
-    for r in range(len(s)+1):
-        p = p.union(set(frozenset(i) for i in combinations(s, r)))
-    return p
-
-# finds the minimal members
-# that is, the members that do contain properly contain another member
-# except for the empty set
-def minimalMembers(family):
-    minimalset = set()
-    for m in family:
-        minimal = True
-        for n in family:
-            if len(n)>0 and (not n is m):
-                if n.issubset(m):
-                    minimal = False
-                    break
-        if minimal:
-            minimalset.add(m)
-    return minimalset
-
-# union of the minimal members
-def minimalElements(family):
-    minElements = set()
-    minMembers = minimalMembers(family)
-    for m in minMembers:
-        for e in m:
-            minElements.add(e)
-    return minElements
-
-#chooses k random subsets from the set {1, ..., n}
-#every subset has the same probability of being chosen
-def randomFamily(n,k):
-    P = powerset(range(1,n+1))
-    return set(sample(P,k))
-
+# family from list of lists
 """
 F = [[1,2],[1],[3],[4,3],[4]]
 F = familyFromList(F)
+"""
+
+
+# makes a random family
+# and makes it union closed
+"""
 F = randomFamily(4,6)
-
-printFamily(F)
-print("union closed: ",isUnionClosed(F))
-print("ground plane: ",getGroundPlane(F))
-print("most apparant:",mostApparant(F),numberOfAppearances(F,mostApparant(F)))
-print("common:",hasCommon(F))
-
 F = makeUnionClosed(F)
+"""
+
+
+#prints information of a family
+"""
 printFamily(F)
 print("union closed: ",isUnionClosed(F))
 print("ground plane: ",getGroundPlane(F))
@@ -124,6 +25,8 @@ print("most apparant:",mostApparant(F),numberOfAppearances(F,mostApparant(F)))
 print("common:",hasCommon(F))
 """
 
+
+# Aproximately finds the amount of families that have a common element
 """
 n = 7
 P = powerset(range(1,n+1))
@@ -139,6 +42,8 @@ for k in range(1,2**n+1):
     print(c/10000)
 """
 
+
+#all kinds of bullshit
 n = 4
 P = powerset(powerset(range(1,n+1)))
 c = 0
